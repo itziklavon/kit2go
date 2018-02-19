78374b4cabeac69324ff65afed2f02abd85ec96f
DESKTOP-2FCUFVA\itzikius <i.lavon@cg.solutions>
2018-02-19 17:18:35 +0200

    test

--- redis-helper/src/redis_helper/RedisConnection.go
+++ redis-helper/src/redis_helper/RedisConnection.go
@@ -18,7 +18,7 @@ func getRedisConnectionByHost(host string) *redis.Client {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-	pong, err := client.Ping().Result()
+	_, err := client.Ping().Result()
 	if err != nil {
 		log.ErrorException(":getRedisConnection: couldn't connect ro redis", err)
 	}
@@ -29,7 +29,7 @@ func Keys() []string {
 	conn := getRedisConnection()
 	value, err := conn.Keys("*")
 	if err != nil {
-		log.ErrorException(":Keys: couldn't get Keys from redis", err)
+		general_log.ErrorException(":Keys: couldn't get Keys from redis", err)
 	}
 	defer conn.Close()
 	return value
@@ -39,7 +39,7 @@ func Keys(pattern string) []string {
 	conn := getRedisConnection()
 	value, err := conn.Keys(pattern)
 	if err != nil {
-		log.ErrorException(":Keys: couldn't get Keys from redis", err)
+		general_log.ErrorException(":Keys: couldn't get Keys from redis", err)
 	}
 	defer conn.Close()
 	return value
@@ -49,7 +49,7 @@ func Get(key string) string {
 	conn := getRedisConnection()
 	value, err := conn.Get(key)
 	if err != nil {
-		log.ErrorException(":Get: couldn't get key from redis: " + key, err)
+		general_log.ErrorException(":Get: couldn't get key from redis: " + key, err)
 	}
 	defer conn.Close()
 	return value
@@ -59,7 +59,7 @@ func Set(key string, value string) string {
 	conn := getRedisConnection()
 	str, err := conn.Set(key, value)
 	if err != nil {
-		log.ErrorException(":Set: couldn't set key from redis: " + key, err)
+		general_log.ErrorException(":Set: couldn't set key from redis: " + key, err)
 	}
 	defer conn.Close()
 	return str
@@ -69,7 +69,7 @@ func HGet(key string, hkey string) string {
 	conn := getRedisConnection()
 	value, err := conn.HGet(key, hkey)
 	if err != nil {
-		log.ErrorException(":Set: couldn't get key from redis: " + key + ", hKey: " + hkey, err)
+		general_log.ErrorException(":Set: couldn't get key from redis: " + key + ", hKey: " + hkey, err)
 	}
 	defer conn.Close()
 	return value
@@ -79,10 +79,10 @@ func HSet(key string, hkey string, value string) {
 	conn := getRedisConnection()
 	str, err := conn.HSet(key, hkey, value)
 	if str {
-		log.ErrorException(":Set: key doesn't exists in redis: " + key + ", hKey: " + hkey, err)
+		general_log.ErrorException(":Set: key doesn't exists in redis: " + key + ", hKey: " + hkey, err)
 	}
 	if err != nil {
-		log.ErrorException(":Set: couldn't get key from redis: " + key + ", hKey: " + hkey, err)
+		general_log.ErrorException(":Set: couldn't get key from redis: " + key + ", hKey: " + hkey, err)
 	}
 	defer conn.Close()
 }
