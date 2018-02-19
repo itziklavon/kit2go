ef9fbfcc35a58ca33b132addac60437ecf14778c
DESKTOP-2FCUFVA\itzikius <i.lavon@cg.solutions>
2018-02-19 16:10:41 +0200

    test

--- app-1/src/main.go
+++ app-1/src/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/itziklavon/kit2go/common/src/common"
+	"github.com/itziklavon/kit2go/redis/src/redis"
 )
 
 func main() {
 	common.PrintHello("world")
+	common.PrintHello(redis.GGetRedisConnection(7))
 }
