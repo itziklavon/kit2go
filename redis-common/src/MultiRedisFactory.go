f48262f2bd0d94242fd6546d44f95c006662ed58
DESKTOP-2FCUFVA\itzikius <i.lavon@cg.solutions>
2018-02-20 09:46:50 +0200

    test

---
+++ redis-common/src/MultiRedisFactory.go
@@ -0,0 +1,39 @@
+package redis_common
+
+
+import (
+	"github.com/itziklavon/kit2go/configuration/src/configuration"
+	"github.com/itziklavon/kit2go/http-client/src/http"
+	"encoding/json"
+)
+
+var DISCOVERY_URL = configuration.GetPropertyValue("DISCOVERY_URL")
+
+
+var redisConnections map[int]string
+
+type RedisData struct {
+	BrandId int `json:"brandId"`
+	Uri string `json:"uri"`
+}
+
+func GetRedisConnection(brandId int) string {
+	if len(redisConnections) == 0 {
+		initMap()
+	}
+	return redisConnections[brandId]
+}
+
+func initMap() {
+	redisConnections = make(map[int]string)
+	url := DISCOVERY_URL + "discovery-web/brand/services/REDIS"
+	httpResponse := http.GETBody(url, nil)
+	var arr []RedisData
+	_ = json.Unmarshal([]byte(httpResponse), &arr)
+	for i := 0; i < len(arr); i = i + 1 {
+		redisConnections[arr[i].BrandId] = arr[i].Uri
+	}
+}
+
+
+
