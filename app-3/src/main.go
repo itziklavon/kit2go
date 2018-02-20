b4cc591abb55f72735c73d91c43c6786040b0253
DESKTOP-2FCUFVA\itzikius <i.lavon@cg.solutions>
2018-02-20 09:36:43 +0200

    test

--- app-3/src/main.go
+++ app-3/src/main.go
@@ -1,6 +1,8 @@
 package main
 
-import ("github.com/itziklavon/kit2go/redis-helper/src/redis_helper")
+import (
+	"github.com/itziklavon/kit2go/redis-helper/src/redis_helper"
+)
 
 func main() {
 	fmt.Printf(redis_helper.GetRedisConnection(7))
