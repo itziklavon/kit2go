38690025d34a5630f7c443fe20db2f0beed9d5c5
DESKTOP-2FCUFVA\itzikius <i.lavon@cg.solutions>
2018-03-06 16:46:23 +0200

    test

--- redis-helper/src/redis_helper/MultyRedisFactory.go
+++ redis-helper/src/redis_helper/MultyRedisFactory.go
@@ -1,1 +1,38 @@
 package redis_helper
+
+import (
+	"encoding/json"
+	"github.com/itziklavon/kit2go/configuration/src/configuration"
+	"github.com/itziklavon/kit2go/http-client-helper/src/http_client_helper"
+)
+
+var DISCOVERY_URL = configuration.GetPropertyValue("DISCOVERY_URL")
+
+var redisConnections map[int]string
+
+type RedisData struct {
+	BrandId int    `json:"brandId"`
+	Uri     string `json:"uri"`
+}
+
+func GetRedisConnection(brandId int) *redis.Client {
+	if len(redisConnections) == 0 {
+		initMap()
+	}
+	return redisConnections[brandId]
+}
+
+func initMap() {
+	redisConnections = make(map[int]string)
+	url := DISCOVERY_URL + "discovery-web/brand/services/REDIS"
+	httpResponse := http_client_helper.GETBody(url, nil)
+	var arr []RedisData
+	_ = json.Unmarshal([]byte(httpResponse), &arr)
+	for i := 0; i < len(arr); i = i + 1 {
+		redisConnections[arr[i].BrandId] = arr[i].Uri
+	}
+}
+
+func getBrandRedisConnection(host string) *redis.Client {
+	return getRedisConnectionByHost(host)
+}
--- redis-helper/src/redis_helper/RedisHelper.go
+++ redis-helper/src/redis_helper/RedisHelper.go
@@ -1,1 +1,73 @@
 package redis_helper
+
+import (
+	"github.com/go-redis/redis"
+	"github.com/itziklavon/kit2go/general-log/src/general_log"
+)
+
+var redisHost = configuration.GetPropertyValue("REDIS_HOST")
+
+func getRedisConnection() *redis.Client {
+	return getRedisConnectionByHost(redisHost)
+}
+
+func getRedisConnectionByHost(host string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     host + ":6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	_, err := client.Ping().Result()
+	if err != nil {
+		general_log.ErrorException(":getRedisConnection: couldn't connect ro redis", err)
+	}
+	return client
+}
+
+func Keys() []string {
+	conn := getRedisConnection()
+	value, err := conn.Keys("*").Result()
+	if err != nil {
+		general_log.ErrorException(":Keys: couldn't get Keys from redis", err)
+	}
+	defer conn.Close()
+	return value
+}
+
+func KeysWithPattern(pattern string) []string {
+	conn := getRedisConnection()
+	value, err := conn.Keys(pattern).Result()
+	if err != nil {
+		general_log.ErrorException(":Keys: couldn't get Keys from redis", err)
+	}
+	defer conn.Close()
+	return value
+}
+
+func Get(key string) string {
+	conn := getRedisConnection()
+	value, err := conn.Get(key).Result()
+	if err != nil {
+		general_log.ErrorException(":Get: couldn't get key from redis: "+key, err)
+	}
+	defer conn.Close()
+	return value
+}
+
+func HGet(key string, hkey string) string {
+	conn := getRedisConnection()
+	value, err := conn.HGet(key, hkey).Result()
+	if err != nil {
+		general_log.ErrorException(":Set: couldn't get key from redis: "+key+", hKey: "+hkey, err)
+	}
+	defer conn.Close()
+	return value
+}
+
+func GetSysParam(hkey string) string {
+	return HGet("SysParams", hkey)
+}
+
+func GetBrandId() string {
+	return GetSysParam("GS_BRAND_ID")
+}
