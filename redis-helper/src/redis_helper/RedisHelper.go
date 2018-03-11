50add6bbbd11f186b462b7b2024e04f04cc11888
DESKTOP-2FCUFVA\itzikius <i.lavon@cg.solutions>
2018-03-11 12:35:46 +0200

    test

--- redis-helper/src/redis_helper/RedisHelper.go
+++ redis-helper/src/redis_helper/RedisHelper.go
@@ -2,7 +2,6 @@ package redis_helper
 
 import (
 	"github.com/go-redis/redis"
-	"github.com/itziklavon/kit2go/configuration/src/configuration"
 	"github.com/itziklavon/kit2go/general-log/src/general_log"
 )
 
